5a3f693c94ef6a1ab741736d0be770adbbca3d98
m1k0de <kim.novac@outlook.com>
2026-09-10 18:56:18 +0200

    Lagt till en lista för varor & en för pris. Med nummer framför vara & pris efter.

--- inkopslistan.cs
+++ inkopslistan.cs
@@ -4,5 +4,18 @@ List<int> prices = new List<int>();
 
 while (true)
 {
-    
+    Console.WriteLine("\nINKÖPSLISTA");
+    for (int i = 0; i < names.Count; i++)
+    {
+        Console.WriteLine($"{i + 1}. {names[i]} - {prices[i]} kr");
+    }
+
+    Console.Write("\nSkriv in varunamn: ");
+    string inputName = Console.ReadLine();
+
+    Console.Write("Skriv in pris i heltal: ");
+    int inputPrice = int.Parse(Console.ReadLine());
+
+    names.Add(inputName);
+    prices.Add(inputPrice);
 }
